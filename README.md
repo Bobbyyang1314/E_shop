6dca7ffc4229352e0877a24224ace398581cf008
Bobby Yang <90142233+Bobbyyang1314@users.noreply.github.com>
2023-07-01 01:55:08 -0400

    9-2 product container

---
+++ README.md
@@ -0,0 +1,9 @@
+# E-shop_App
+
+Plugins installation
+
+npm install mongoose express cors dotenv morgan nodemon bcryptjs jsonwebtoken express-jwt
+
+Start backend server
+
+npm start
---
+++ easy-shop/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+// Screens
+import ProductContainer from './Screens/Products/ProductContainer';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ProductContainer />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
