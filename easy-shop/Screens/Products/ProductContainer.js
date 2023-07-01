dcc7386f2e998881aae352677fba0aa1854bdec2
Bobby Yang <90142233+Bobbyyang1314@users.noreply.github.com>
2023-07-01 02:34:18 -0400

    9-3 over

--- easy-shop/Screens/Products/ProductContainer.js
+++ easy-shop/Screens/Products/ProductContainer.js
@@ -1,10 +1,35 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, ActivityIndicator, Text } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, StyleSheet, ActivityIndicator, Text, FlatList } from 'react-native';
+
+import ProductList from './ProductList';
+
+const data = require('../../assets/data/products.json');
 
 const ProductContainer = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        setProducts(data);
+        return () => {
+            setProducts([])
+        }
+    }, [])
+
     return (
         <View>
             <Text>Product Container</Text>
+            <View style={{marginTop: 100}}>
+                <FlatList
+                    horizontal
+                    data={products}
+                    renderItem={({item}) => <ProductList
+                        key={item.id}
+                        item={item}/>}
+                    keyExtractor={item => item.name}
+                />
+            </View>
+
         </View>
     )
 }
