ec1975cc2c665df463ab9dfab164fd915774400b
KhnNord <rangerokgu@gmail.com>
2023-07-03 18:26:33 -0400

    SearchedProducts.js

--- easy-shop/Screens/Products/SearchedProducts.js
+++ easy-shop/Screens/Products/SearchedProducts.js
@@ -3,4 +3,46 @@ import { View, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
 import { Content, Left, Body, ListItem, Thumbnail, Text} from 'native-base';
 
 
-const SearchedProduct = (props)
+const SearchedProduct = (props) => {
+    const { productsFiltered } = props;
+    return(
+        <Content>
+            {productsFiltered.length > 0 ? (
+                productsFiltered.map((item) => (
+                    <ListItem
+                        // onPress={navigation}
+                        key={item._id.$oid}
+                        avatar
+                    >
+                        <Left>
+                            <Thumbnail
+                                source={{uri: item.image ? 
+                                    item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
+                                    }}
+                            />
+                        </Left>
+                        <Body>
+                            <Text>{item.name}</Text>
+                            <Text note>{item.description}</Text>
+                        </Body>
+                    </ListItem>
+                ))
+            ) : (
+                <View style={StyleSheet.center}>
+                    <Text style={{alignSelf: 'center'}}>
+                        No products match the selected criteria
+                    </Text>
+                </View>
+            )}
+        </Content>
+    );
+};
+
+const styles = Stylesheet.create({
+    center: {
+        justifyContent: 'center',
+        aalignItems: 'center'
+    }
+})
+
+export default SearchedProduct;
