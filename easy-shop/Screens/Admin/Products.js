605a9f968f50d8bdb1c13b1156b3951518d97c21
Bobby Yang <90142233+Bobbyyang1314@users.noreply.github.com>
2023-07-23 22:42:47 -0400

    Section18 - 1 over Bug：composition and press onFocus dashboard doesn't work Checkout Confirm.js doesn't show images and text Auth has problems with the version, UserProfile cannot show Products.js: renderItem cannot render ListItem.js

--- easy-shop/Screens/Admin/Products.js
+++ easy-shop/Screens/Admin/Products.js
@@ -1,10 +1,106 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useCallback } from "react";
+import {View, Text, FlatList, ActivityIndicator, StyleSheet, Dimensions, Image} from "react-native";
+import {Input, VStack} from "native-base";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useFocusEffect } from "@react-navigation/native";
+
+import axios from "axios";
+import baseURL from "../../assets/common/baseUrl";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {Ionicons} from "@expo/vector-icons";
+
+import ListItem from "./ListItem";
+
+
+
+const { height, width } = Dimensions.get("window");
+
 
 const Products = (props) => {
+
+    const [productList, setProductList] = useState();
+    const [productFilter, setProductFilter] = useState();
+    const [loading, setLoading] = useState(true);
+    const [token, setToken] = useState();
+
+    useFocusEffect(
+        useCallback(
+            () => {
+                // Get Token
+                AsyncStorage.getItem("jwt")
+                    .then((res) => {
+                        setToken(res);
+                    })
+                    .catch((error) => console.log(error))
+
+                axios
+                    .get(`${baseURL}products`)
+                    .then((res) => {
+                        setProductList(res.data);
+                        setProductFilter(res.data);
+                        setLoading(false);
+                    })
+                return () => {
+                    setProductList();
+                    setProductFilter();
+                    setLoading(true);
+                }
+            }, [],
+        )
+    )
+
     return (
         <View>
-            <Text>Pro</Text>
+            <View>
+                <VStack space={2} alignItems="center" mt={4}>
+                    <Input
+                        placeholder="Search"
+                        // onChangeText={(text) => searchProduct(text)}
+                        variant="filled"
+                        width="100%"
+                        borderRadius="10"
+                        py="1"
+                        px="2"
+                        InputLeftElement={
+                            <Icon ml="2" size="4" color="gray.400" as={<Ionicons name="ios-search" />} />
+                        }
+                        // rightElement={<Text
+                        //     onPress={onBlur}
+                        //     marginRight={3}
+                        //     outlineColor={"black"}
+                        //     color={"gray.400"}>Back</Text>}
+                    />
+                </VStack>
+            </View>
+            {loading ? (
+                <View>
+                    <ActivityIndicator size="large" color="red"/>
+                </View>
+            ) : (
+                <FlatList
+                    data={productFilter}
+                    renderItem={({ item, index }) => (
+                        // <ListItem
+                        //     {...item}
+                        //     navigation={props.navigation}
+                        //     index={index}
+                        // />
+                        <View>
+                            <Image source={{
+                                uri: item.image
+                                    ? item.image
+                                    : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'}}
+                                   resizeMode="contain"
+                            />
+                            <Text>{item.brand}</Text>
+                            <Text numberOfLines={1} ellipsizeMode="tail">{item.name}</Text>
+                            <Text numberOfLines={1} ellipsizeMode="tail">{item.category.name}</Text>
+                            <Text>${item.price}</Text>
+                        </View>
+                    )}
+                    keyExtractor={(item) => item.id}
+                />
+            )}
         </View>
     )
 }
