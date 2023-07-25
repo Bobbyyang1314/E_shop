379f1fc39275d4d98824b746fe7910a83a47588d
Bobby Yang <90142233+Bobbyyang1314@users.noreply.github.com>
2023-07-25 14:43:08 -0400

    Section18 over Bug：composition and press onFocus dashboard doesn't work Checkout Confirm.js doesn't show images and text Auth has problems with the version, UserProfile cannot show Admin Cannot search

--- easy-shop/Screens/Admin/Products.js
+++ easy-shop/Screens/Admin/Products.js
@@ -15,6 +15,31 @@ import ListItem from "./ListItem";
 
 const { height, width } = Dimensions.get("window");
 
+const ListHeader = () => {
+    return (
+        <View
+            elevation={1}
+            style={styles.listHeader}
+        >
+            <View style={styles.headerItem}>
+
+            </View>
+            <View style={styles.headerItem}>
+                <Text style={styles.boldHeader}>Brand</Text>
+            </View>
+            <View style={styles.headerItem}>
+                <Text style={styles.boldHeader}>Name</Text>
+            </View>
+            <View style={styles.headerItem}>
+                <Text style={styles.boldHeader} numberOfLines={1}>Category</Text>
+            </View>
+            <View style={styles.headerItem}>
+                <Text style={styles.boldHeader}>Price</Text>
+            </View>
+        </View>
+    )
+}
+
 
 const Products = (props) => {
 
@@ -49,21 +74,32 @@ const Products = (props) => {
         )
     )
 
+    const searchProduct = (text) => {
+        if (text === "") {
+            setProductFilter(productList);
+        }
+        setProductFilter(
+            productList.filter((i) => {
+                i.name.toLowerCase().includes(text.toLowerCase())
+            })
+        )
+    }
+
     return (
         <View>
             <View>
                 <VStack space={2} alignItems="center" mt={4}>
                     <Input
                         placeholder="Search"
-                        // onChangeText={(text) => searchProduct(text)}
+                        onChangeText={(text) => searchProduct(text)}
                         variant="filled"
                         width="100%"
                         borderRadius="10"
                         py="1"
                         px="2"
-                        InputLeftElement={
-                            <Icon ml="2" size="4" color="gray.400" as={<Ionicons name="ios-search" />} />
-                        }
+                        // InputLeftElement={
+                        //     <Icon ml="2" size="4" color="gray.400" as={<Ionicons name="ios-search" />} />
+                        // }
                         // rightElement={<Text
                         //     onPress={onBlur}
                         //     marginRight={3}
@@ -73,12 +109,13 @@ const Products = (props) => {
                 </VStack>
             </View>
             {loading ? (
-                <View>
+                <View style={styles.spinner}>
                     <ActivityIndicator size="large" color="red"/>
                 </View>
             ) : (
                 <FlatList
                     data={productFilter}
+                    ListHeaderComponent={ListHeader}
                     renderItem={({ item, index }) => (
                         <ListItem
                             {...item}
@@ -92,5 +129,25 @@ const Products = (props) => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    listHeader: {
+        flexDirection: "row",
+        padding: 5,
+        backgroundColor: "gainsboro"
+    },
+    headerItem: {
+        margin: 3,
+        width: width / 6
+    },
+    boldHeader: {
+        fontWeight: "bold"
+    },
+    spinner: {
+        height: height / 2,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+})
 
 export default Products;
